6faed4fc1e560faf0c35adaccd2dc2dca861fdb0
josephxia <15906475@qq.com>
2023-05-09 21:54:23 +0800

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionFilter } from './shared/filters/AllExceptionFilter';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService,],
+  providers: [AppService, {
+    provide: APP_FILTER,
+    useClass: AllExceptionFilter,
+  },],
 })
 export class AppModule { }
