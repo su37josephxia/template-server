d9dac868e53f5caae011256f328dfe5283dd8297
josephxia <15906475@qq.com>
2023-09-20 20:28:43 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { generateDocument } from './doc';
 import { RemoveSensitiveUserInfoInterceptor } from './shared/interceptors/remove-sensitive-info.interceptor';
-
+import helmet from 'helmet'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet({}))
 
   app.useGlobalPipes(new ValidationPipe())
 
