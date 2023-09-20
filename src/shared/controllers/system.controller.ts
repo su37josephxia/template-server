b808699efc4722f4b5ae3e37de2dd038350dd591
josephxia <15906475@qq.com>
2023-09-20 20:17:31 +0800

    update

--- src/shared/controllers/system.controller.ts
+++ src/shared/controllers/system.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from "@nestjs/common";
-import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { spawn } from 'child_process'
+import * as moment from "moment";
+import { BackupDto } from "../dtos/backup.dto";
 
 @ApiTags('系统维护')
 @Controller('system')
@@ -17,7 +19,7 @@ export class SystemController {
         const ret = await this.spawn('docker-compose', ['exec', '-T', 'mongo', 'ls', '/dump'], { cwd: './' })
         console.log(ret)
         const data = ("" + ret).split('\n')
-        // data.pop()
+        data.pop()
         return {
             ok: 1,
             data
@@ -39,5 +41,30 @@ export class SystemController {
                 resolve(ret)
             })
         })
+    }
+
+
+    @ApiOperation({
+        summary: '数据库备份',
+    })
+    @Post('/database/dump')
+    async dump(@Body() data) {
+        const ret = await this.spawn('docker-compose', ['exec', '-T', 'mongo', 'mongodump', '--db', 'nest-server', '--out', '/dump/' + moment().format('YYYYMMDDhhmmss')], { cwd: './' })
+        return {
+            ok: 1
+        }
+    }
+
+    @ApiOperation({
+        summary: '数据库恢复',
+    })
+    @Post('/database/restore')
+    async restore(@Body() dto: BackupDto) {
+        console.log('恢复数据', dto.file)
+        const ret = await this.spawn('docker-compose', ['exec', '-T', 'mongo', 'mongorestore', '--db', 'nest-server', `/dump/${dto.file}/nest-server`], { cwd: './' })
+
+        return {
+            ok: 1
+        }
     }
 }
---
+++ src/shared/dtos/backup.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, Matches } from 'class-validator';
+
+export class BackupDto {
+
+    @ApiProperty({ example: 'init' })
+    @IsNotEmpty()
+    file?: string;
+
+}
