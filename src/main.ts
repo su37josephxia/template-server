a355861e145b3af07060818efbbcbbf068e2a0d4
josephxia <15906475@qq.com>
2023-09-20 20:50:36 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { generateDocument } from './doc';
 import { RemoveSensitiveUserInfoInterceptor } from './shared/interceptors/remove-sensitive-info.interceptor';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
 
   app.useGlobalPipes(new ValidationPipe())
 
