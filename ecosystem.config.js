a5d167cd02ff2f5b42dc6f0219e71a2181be45c1
xiaran01 <xiaran01@corp.netease.com>
2024-06-14 11:07:48 +0800

    add eco

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    server: [
+        {
+            name: "template-server",
+            script: "./process.yml",
+        },
+    ],
+    ssr: [
+        {
+            name: "builder-ssr",
+            script: "./builder-ssr/process.yml",
+        },
+    ],
+    ssg: [
+        {
+            name: "builder-ssg",
+            script: "./builder/process.yml",
+        },
+    ],
+};
